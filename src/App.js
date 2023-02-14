d5c7bf2c1e95c569e81e8801635af58453dbc152
Rachel <racheljay42@gmail.com>
2023-02-13 17:48:04 -0800

    basic setup, word list, random word generator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import { styled } from '@stitches/react';
+import {fourLetterWords} from './data/four-letter-words'
+
+/* Styled Components */
+
+const Container = styled('div', {
+  backgroundColor: "pink",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  height: "100vh",
+  margin: "0 auto"
+})
+
+const Title = styled('h1', {
+  color: "orange",
+  margin: "0 auto"
+})
+const Word = styled('div', {
+  fontSize: "30px"
+})
+
+// TODO: save chosen word in storage?
+// make input for guess word
+  // limit to four chars
+// write logic to compare guess word and secret word
+
+
+const randomIndex = Math.floor(Math.random() * fourLetterWords.length)
+const word = fourLetterWords[randomIndex]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Title>Wordle clone</Title>
+      <Word>{word}</Word>
+    </Container>
   );
 }
 
