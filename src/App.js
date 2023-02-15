5bb5cf56373c67ad70bbcdb46e3c035c5895edde
Rachel <racheljay42@gmail.com>
2023-02-14 16:16:54 -0800

    input and submit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { styled } from '@stitches/react';
-import {fourLetterWords} from './data/four-letter-words'
+import { fourLetterWords } from './data/four-letter-words'
+import { useState } from 'react';
 
 /* Styled Components */
 
@@ -21,21 +22,54 @@ const Word = styled('div', {
   fontSize: "30px"
 })
 
+const WordInput = styled('input', {
+
+})
+
+const SubmitButton = styled('button', {
+
+})
+
+
 // TODO: save chosen word in storage?
 // make input for guess word
-  // limit to four chars
+// limit to four chars
 // write logic to compare guess word and secret word
 
-
-const randomIndex = Math.floor(Math.random() * fourLetterWords.length)
-const word = fourLetterWords[randomIndex]
-
-
 function App() {
+  const randomIndex = Math.floor(Math.random() * fourLetterWords.length)
+  const word = fourLetterWords[randomIndex]
+  const [guess, setGuess] = useState('')
+
+  const handleClick = () => {
+    if (guess.length === 4) {
+      console.log(guess)
+    } else if (guess.length < 4) {
+      console.log('guess is too short')
+    }
+  }
+
+  const validGuess = () => {
+    return guess.length === 4 ? true : false
+  }
   return (
     <Container>
       <Title>Wordle clone</Title>
       <Word>{word}</Word>
+      <WordInput
+        maxLength={4}
+        minLength={4}
+        onChange={e => {
+          setGuess(e.target.value)
+          console.log(guess)
+        }}
+        type="text"
+      ></WordInput>
+      <SubmitButton
+        type="submit"
+        onClick={() => handleClick()}
+        disabled={!validGuess()}
+      >Check Word</SubmitButton>
     </Container>
   );
 }
